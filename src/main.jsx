201ebfa40f34a0cdcd2232ea5cbabfe206a9c2a6
Bruna Pessoa <brunapessoa.bpm@gmail.com>
2026-05-15 14:02:42 +0200

    Configure base path and router for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const root = document.getElementById("root");
 
 createRoot(root).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/to-do-list-react">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/details" element={<DetailsTask />}></Route>
