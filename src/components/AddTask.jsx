854ad7b83434551b7d272eaa2a365d3deaf803eb
Bruna Pessoa <brunapessoa.bpm@gmail.com>
2026-05-17 12:41:34 +0200

    testing for mobile

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -5,12 +5,12 @@ function AddNewTask({ addTaskOnList }) {
   const [description, setDescription] = useState("");
 
   return (
-    <div className="flex bg-mauve-200 space-y-5 mx-auto max-w-md p-10 rounded-2xl flex-col shadow-md">
+    <div className="flex bg-mauve-200 space-y-5 my-10 mx-auto max-w-md py-10 px-5 rounded-2xl flex-col shadow-md">
       <input
         type="text"
         placeholder="Type your new task"
         id="newTask"
-        className="bg-white border-mauve-800 border-2 focus:outline-mauve-800  py-0.5 px-1.5 rounded-md"
+        className="bg-white border-mauve-800 border-2 focus:outline-mauve-800 py-0.5 px-1.5 rounded-md"
         value={title}
         onChange={function addTitle(event) {
           setTitle(event.target.value);
